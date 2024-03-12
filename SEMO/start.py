f4e8afde6a6e86eecc095395779bd211053d9fa1
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-12 19:29:21 +0200

    Update start.py

--- SEMO/start.py
+++ SEMO/start.py
@@ -1588,4 +1588,53 @@ async def wel__come(client: Client, message):
 async def good_bye(client: Client, message):
 	chatid= message.chat.id
 	await client.send_message(text=f"كنت راجل محترم يا  {message.from_user.mention} ",chat_id=chatid)
+
+iddof = []
+@app.on_message(filters.command(["قفل جمالي","تعطيل جمالي"], ""))
+async def lllock(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if message.chat.id in iddof:
+        return await message.reply_text("جمالي معطل من قبل✅")
+      iddof.append(message.chat.id)
+      return await message.reply_text(" تم تعطيل جمالي بنجاح✅🔒")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+
+@app.on_message(filters.command(["فتح جمالي","تفعيل جمالي"], ""))
+async def idljjopen(client, message):
+   get = await app.get_chat_member(message.chat.id, message.from_user.id)
+   if get.status in ["creator", "administrator"]:
+      if not message.chat.id in iddof:
+        return await message.reply_text("جمالي مفعل من قبل✅")
+      iddof.remove(message.chat.id)
+      return await message.reply_text("تم فتح جمالي بنجاح ✅🔓")
+   else:
+      return await message.reply_text("لازم تكون ادمن يشخه علشان اسمع كلامك")
+
+
+
+
+@app.on_message(filters.command(["جمالي"], ""))
+async def idjjdd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
+    ik = random.choice(i)
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"نسبه جمالك يا مز انت \n│ \n└ʙʏ: {ik} %😂❤️", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        ),
+    )
+       
+
+
 	
