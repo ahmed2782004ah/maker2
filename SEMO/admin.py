959a4f0cd2d43a284370e1afe0edc1f56638aa35
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-13 15:07:34 +0200

    Update admin.py

--- SEMO/admin.py
+++ SEMO/admin.py
@@ -159,8 +159,8 @@ async def admin_risght(client: Client, message):
        requester = userx.mention       
        gr = await get_group(bot_username)
        ch = await get_channel(bot_username)
-       button = [[InlineKeyboardButton(text="「𝚂𝚃𝙾𝙿」", callback_data=f"stop"), InlineKeyboardButton(text="RESUME", callback_data=f"「𝚁𝙴𝚂𝚞𝙼𝙴」"), InlineKeyboardButton(text="「𝙿𝙰𝚞𝚂𝙴」", callback_data=f"pause")], [InlineKeyboardButton(text="「𝙲𝙷𝙰𝙽𝙽𝙴𝙻」", url=f"{ch}"), InlineKeyboardButton(text="「𝙶𝚁𝙾𝚞𝙿」", url=f"{gr}")], [InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/A_q_lp")], [InlineKeyboardButton(text="اضف البوت الي مجموعتك او قناتك ⚡", url=f"https://t.me/{bot_username}?startgroup=True")]]
-       await message.reply_photo(photo=img, caption=f"**Skipped Streaming **\n\n**Song Name** : {title}\n**Duration Time** {dur}\n**Request By** : {requester}", reply_markup=InlineKeyboardMarkup(button))
+       button = [[InlineKeyboardButton(text="「𝚂𝚃𝙾𝙿」", callback_data=f"stop"), InlineKeyboardButton(text="「𝚁𝙴𝚂𝚞𝙼𝙴」", callback_data=f"RESUME"), InlineKeyboardButton(text="「𝙿𝙰𝚞𝚂𝙴」", callback_data=f"pause")], [InlineKeyboardButton(text="「𝙲𝙷𝙰𝙽𝙽𝙴𝙻」", url=f"{ch}"), InlineKeyboardButton(text="「𝙶𝚁𝙾𝚞𝙿」", url=f"{gr}")], [InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/VVYVVJ")], [InlineKeyboardButton(text="اضف البوت الي مجموعتك او قناتك ⚡", url=f"https://t.me/{bot_username}?startgroup=True")]]
+       await message.reply_photo(photo=img, caption=f"**Skipped Streaming **\n\n**العنوان** : {title}\n**المدة** {dur}\n**طلب بواسطة** : {requester}", reply_markup=InlineKeyboardMarkup(button))
        try:
            os.remove(file_path)
            os.remove(img)
